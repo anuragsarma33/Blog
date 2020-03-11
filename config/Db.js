c49f5ba6387e18d124e0d84a1bfbe14cfc04b4e3
Anurag Sarma <anuragsarma33@gmail.com>
2020-03-11 20:06:25 +0530

    Initial Commit

---
+++ config/Db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    DB: "mongodb+srv://user1:cjsIElBlbWsEPAxv@cluster0-oemcm.mongodb.net/Blog?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
+}
