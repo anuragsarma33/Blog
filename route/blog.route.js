181e901010fc8e57167177fd031d34b6f7d2f1bb
Anurag Sarma <anuragsarma33@gmail.com>
2020-03-12 09:54:00 +0530

    Route

--- route/blog.route.js
+++ route/blog.route.js
@@ -5,5 +5,8 @@ const appControl = require('../controller/blog.controller');
 appRoute.get('/', appControl.index);
 appRoute.get('/create', appControl.create);
 appRoute.post('/create', appControl.viewPost);
+appRoute.get('/edit/:id', appControl.editPost);
+appRoute.post('/edit/:id', appControl.updatePost);
+appRoute.get('/delete/:id', appControl.deletePost);
 
 module.exports = appRoute;
